336a3d27af753c1ca50b56e47d6056223854567c
Suvanwita <dsuvanwita@gmail.com>
2026-05-15 19:09:29 +0530

    clean up

--- target/debug/deps/unicode_ident-419388df86723a00.d
+++
@@ -1,8 +0,0 @@
-/home/user/ByteLens/target/debug/deps/unicode_ident-419388df86723a00.d: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/user/ByteLens/target/debug/deps/libunicode_ident-419388df86723a00.rlib: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/user/ByteLens/target/debug/deps/libunicode_ident-419388df86723a00.rmeta: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
